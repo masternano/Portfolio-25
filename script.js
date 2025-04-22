ac04d4442bb9457694da3c1434c0c022d097a7cd
Masternano <samuelnnabude@gmail.com>
2025-04-22 10:04:16 +0100

    not commits

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const projectCards = [
           id: 1,
           title: "Title",
           imageSrc: "/img/project.png",
-      //     subtractSrc: "/subtract-2.svg",
+          subtractSrc: "/subtract-2.svg",
           rotation: "rotate-left"
       },
       {
           id: 2,
           title: "Title",
           imageSrc: "/img/project.png",
-      //     subtractSrc: "/subtract-1.svg",
+          subtractSrc: "/subtract-1.svg",
           rotation: "rotate-right"
       },
       {
           id: 3,
           title: "Title",
           imageSrc: "/img/project.png",
-      //     subtractSrc: "/subtract.svg",
+          subtractSrc: "/subtract.svg",
           rotation: "rotate-left"
       }
   ];
@@ -35,7 +35,7 @@ const projectCards = [
               <div class="card">
                   <div class="card-content">
                       <img class="project-image" src="${card.imageSrc}" alt="${card.title} image">
-                      <img class="project-overlay" src="${card.subtractSrc}" alt="">
+                      <img class="project-overlay" src="${card.subtractSrc}" alt="${card.title} overlay">
                   </div>
               </div>
               <div class="project-title">${card.title}</div>
